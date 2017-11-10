6dd2a4e386b6a122921ff0d41ff346d3c1a107bc
jpvg10 <juan.valencia10a@gmail.com>
2017-11-10 08:59:34 -0500

    Validate email

--- src/components/signup/index.js
+++ src/components/signup/index.js
@@ -7,25 +7,33 @@ import UserForm from '../shared/user-form';
 class Register extends React.Component {
 	// Custom methods
 	onSubmit(email, password) {
-		let options = {
-			method: 'post',
-			url: '/auth/signup',
-			data: { 
-				email: email,
-				password: password 
-			} 
-		};
-		axios.request(options)
-			.then(function(response){				
-				localStorage.setItem('successMessage', 'You have successfully signed up. You can now log in');
-				this.props.history.push('/login');				
-			}.bind(this))
-			.catch(function(error) {
-				this.setState({
-					message: error.response.data.message,
-					success: error.response.data.success
-				});
-			}.bind(this));
+		let re = /\S+@\S+\.\S+/;
+		if(re.test(email)){
+			let options = {
+				method: 'post',
+				url: '/auth/signup',
+				data: { 
+					email: email,
+					password: password 
+				} 
+			};
+			axios.request(options)
+				.then(function(response){				
+					localStorage.setItem('successMessage', 'You have successfully signed up. You can now log in');
+					this.props.history.push('/login');				
+				}.bind(this))
+				.catch(function(error) {
+					this.setState({
+						message: error.response.data.message,
+						success: error.response.data.success
+					});
+				}.bind(this));
+		}else{
+			this.setState({
+				message: 'Please use a valid email address',
+				success: false
+			});
+		}
 	}
 
 	// Component methods
@@ -43,7 +51,10 @@ class Register extends React.Component {
 		return(
 			<div>
 				<h1 className="title">Sign up</h1>
-				<UserForm title="Sign up" onSubmit={this.onSubmit} message={this.state.message} error={this.state.success} />
+				<div className="content is-small">
+					<span>You can use a fake email, but shh, don't tell anyone</span>
+				</div>
+				<UserForm title="Sign up" onSubmit={this.onSubmit} message={this.state.message} success={this.state.success} />
 			</div>
 		);
 	}
