6f1531bb902bdf0bd836b705d2df9ee401675f28
Juan Pablo Valencia <juan.valencia10a@gmail.com>
2022-08-31 15:40:32 +0300

    configuring prettier

--- server/src/interfaces.ts
+++ server/src/interfaces.ts
@@ -18,7 +18,7 @@ export interface IMovie {
   _user: Types.ObjectId;
 }
 
-export interface IMovieDocument extends Document {}
+export type IMovieDocument = Document;
 
 export interface IPayload {
   _id: string;
